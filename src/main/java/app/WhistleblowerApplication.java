92e119d8d4949cf075a23499cdb26f0de4409959
Maksym Rohachov <romax982@gmail.com>
2024-01-13 17:35:07 +0100

    Update db configs

--- src/main/java/app/WhistleblowerApplication.java
+++ src/main/java/app/WhistleblowerApplication.java
@@ -2,8 +2,10 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("src/main/java/app/entity")
 public class WhistleblowerApplication {
     public static void main(String[] args) {
         SpringApplication.run(WhistleblowerApplication.class, args);
