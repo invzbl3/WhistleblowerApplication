e421a8e3cbb80ad430eb45da1a967c66a962655c
Maksym Rohachov <romax982@gmail.com>
2024-01-14 19:28:22 +0100

    Remove unused annotations according to jakarta.persistence-api dependency

--- src/main/java/app/WhistleblowerApplication.java
+++ src/main/java/app/WhistleblowerApplication.java
@@ -2,16 +2,8 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@SpringBootApplication
-//@EntityScan("src/main/java/app/entity")
-//@EntityScan(basePackages = {"app.entity"})
-//@EnableJpaRepositories(basePackages = {"app.repository"})
-@SpringBootApplication(scanBasePackages = {"app"})
-@EntityScan("app.entity")
-@EnableJpaRepositories("app.repository")
+@SpringBootApplication
 public class WhistleblowerApplication {
     public static void main(String[] args) {
         SpringApplication.run(WhistleblowerApplication.class, args);
