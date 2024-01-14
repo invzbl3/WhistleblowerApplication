ad42befe834e834b265c7989db3883e05548ba50
Maksym Rohachov <romax982@gmail.com>
2024-01-14 17:30:42 +0100

    Update WhistleblowerApplication

--- src/main/java/app/WhistleblowerApplication.java
+++ src/main/java/app/WhistleblowerApplication.java
@@ -7,8 +7,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 //@EntityScan("src/main/java/app/entity")
-@EntityScan(basePackages = {"app.entity"})
-@EnableJpaRepositories(basePackages = {"app.repository"})
+//@EntityScan(basePackages = {"app.entity"})
+//@EnableJpaRepositories(basePackages = {"app.repository"})
+@EntityScan("app.entity")
+@EnableJpaRepositories("app.repository")
 public class WhistleblowerApplication {
     public static void main(String[] args) {
         SpringApplication.run(WhistleblowerApplication.class, args);
